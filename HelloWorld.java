ba906269cd5dc824969890af3ee70b139726e35f
robertmchugh <42556355+robertmchugh@users.noreply.github.com>
2018-08-20 17:42:15 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,16 @@ public class Main
 
    public static void main(String[] args)
    {
-      System.out.println("hello world");
+    System.out.print("Here are some famous and motivating ");
+    System.out.println("quotes from the famous NBA player, Lebron James:");
+    System.out.println();
+    System.out.println("1. You have to be able to accept failure to get better.");
+    System.out.println("2. I treated it like everyday was my last day with a basketball.");
+    System.out.print("3. I'm going to use all my tools, my God-given ability, ");
+    System.out.println("and make the best life I can make it.");
+    System.out.println();
+    System.out.print("I hope these quotes from Lebron James will motivate you");
+    System.out.println(" just as much as they have motivated me!");
    }
 
 }
